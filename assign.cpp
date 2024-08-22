619fc726b29a3ccd26fff89962b7d19e7f357bb6
Omveshsingh <148647255+Omveshsingh@users.noreply.github.com>
2024-08-22 23:13:19 +0530

    Add files via upload

---
+++ assign.cpp
@@ -0,0 +1,14 @@
+//Q1.
+#include<iomanip>
+#include<string>
+#include<iostream>
+using namespace std;
+int main(){
+  string first,second;
+  cout <<"Enter the first string: "<<endl;
+  cin>>first;
+  cout<<"Enter the second string: "<<endl;
+  cin>>second;
+  cout<< "Concatenated string: "<< first <<second;
+  return 0;
+}
---
+++ question 3.cpp
@@ -0,0 +1,12 @@
+//Q3.
+#include<iomanip>
+#include<string>
+#include<iostream>
+using namespace std;
+int main(){
+    int input;
+    cout <<"Enter a number: "<<endl;
+    cin>>input;
+    cout<<"The square of "<<input<<" is:"<<input*input<<endl;
+    return 0;
+}
